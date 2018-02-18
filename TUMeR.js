60efbf3ce1acf84af6f6c83ef4a40df1fd213ee9
zsewa <zsewa@outlook.de>
2018-02-18 21:24:09 +0100

    added copyright notice (MIT)

--- TUMeR.js
+++ TUMeR.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            TUM eAccess Redirector
 // @name:de         TUM eAccess Redirector
-// @version         1.0.1.8
+// @version         1.0.1.9
 // @description     Automatically redirect to the proxified equivalent as provided by the libary of the Technische Universitaet Muenchen via the eAccess-System. TUM credentials required.
 // @description:de  Userscript, dass auf einigen Seiten, mit denen die Universitätsbiblothek der Technischen Universität München kooperiert, die eAccess-Version aufruft.
 // @author          zsewa
@@ -97,6 +97,18 @@
 // @grant GM_registerMenuCommand
 
 // ==/UserScript==
+
+/*
+This file is licenced under a MIT licence.
+
+Copyright 2017, 2018 zsewa <zsewa@outlook.de>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 /*
 not working (external access permitted):
